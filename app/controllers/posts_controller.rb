0860e19597741e5cdecd89b95b488d69c3f3bfa3
Sam Wu <sam.x.wu@gmail.com>
2017-01-06 23:13:54 +0000

    Checkpoint 18 Rails Intro CRUD Assignment Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index { |value, index| value.title = "SPAM" if index+1 == 1 || (index+1)%5 == 0 }
   end
 
   def show
