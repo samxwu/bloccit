02456a87c3fbe63cf6a3866a717a86d95a58284d
Sam Wu <sam.x.wu@gmail.com>
2017-01-22 15:14:53 +0000

    Checkpoint 22 Rails Topics Posts Assignment Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+#  get 'sponsored_posts/show'
+
+#  get 'sponsored_posts/new'
+
+#  get 'sponsored_posts/edit'
+
 #  get 'posts/index'
 
 #  get 'posts/show'
@@ -6,6 +12,10 @@ Rails.application.routes.draw do
 #  get 'posts/new'
 
 #  get 'posts/edit'
+
+resources :topics do
+  resources :sponsored_posts, except: [:index]
+end
 
 resources :topics do
     resources :posts, except: [:index]
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,9 +27,19 @@ topics = Topic.all
         )
     end
     
-
+#Create SponsoredPosts
+20.times do
+    SponsoredPost.create!(
+        topic: topics.sample,
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        price: 99
+        )
+    end
+    sponsoredposts = SponsoredPost.all
 
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsoredposts created"
