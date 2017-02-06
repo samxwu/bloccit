a54b42c12d120024287f29b3179733f7c5ef5816
Sam Wu <sam.x.wu@gmail.com>
2017-02-06 21:30:11 +0000

    Checkpoint 29 Rails Comments

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ resources :topics do
     resources :posts, except: [:index]
 end
 
+resources :posts, only: [] do
+  resources :comments, only: [:create, :destroy]
+end
+   
+   
 resources :users, only: [:new, :create]
 resources :sessions, only: [:new, :create, :destroy]
 
