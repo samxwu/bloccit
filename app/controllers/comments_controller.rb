fa16f7c74449d9aad21714231492c7efb6819ed0
Sam Wu <sam.x.wu@gmail.com>
2017-02-07 19:13:08 +0000

    Revised Checkpoint 29

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,7 @@ class CommentsController < ApplicationController
    before_action :require_sign_in
    before_action :authorize_user, only: [:destroy]
  
-   def createdef create
-     @post = Post.find(params[:post_id])
-     comment = @post.comments.new(comment_params)
-     comment.user = current_user
- 
-     if comment.save
-       flash[:notice] = "Comment saved successfully."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment failed to save."
-       redirect_to [@post.topic, @post]
-     end
-   end
+   def create
      @post = Post.find(params[:post_id])
      comment = @post.comments.new(comment_params)
      comment.user = current_user
